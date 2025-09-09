5ce3912c4b719db07200e7e920f01c0d3d34af88
ljhee92 <weilainib7@gmail.com>
2025-09-09 13:05:43 +0900

    feat: Thread 학습 테스트 추가

--- study/src/test/java/thread/stage0/SynchronizationTest.java
+++ study/src/test/java/thread/stage0/SynchronizationTest.java
@@ -41,8 +41,10 @@ class SynchronizationTest {
 
         private int sum = 0;
 
-        public void calculate() {
-            setSum(getSum() + 1);
+        public void calculate() { // or public synchronized void calculate()
+            synchronized (this) {
+                setSum(getSum() + 1);
+            }
         }
 
         public int getSum() {
